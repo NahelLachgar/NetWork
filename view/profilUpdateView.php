537ad733ee8660b0301f881c4bafc4cfe7c8de58
Kevin Barao Da Silva <baraodasilva@intechinfo.fr>
2018-12-11 11:55:26 +0100

    modif

--- view/profilUpdateView.php
+++ view/profilUpdateView.php
@@ -94,10 +94,6 @@ ob_start();
               <span id="aidePhone"></span>
             </div>
         </div>
-
-
-
-
             <hr class="mb-4">
             <div class="row">
               <div class="col-md-6 mb-3">
