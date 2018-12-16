3a35516b7cccbc8fd9559d809ac2635b28640e1e
NahelLachgar <nahellachgar@hotmail.fr>
2018-12-16 17:53:50 +0100

    jquery chat

--- view/chatView.php
+++ view/chatView.php
@@ -7,7 +7,7 @@ ob_start();
 		<div id="profile">
 			<div class="wrap">
 				<img id="profile-img" src="https://picsum.photos/50/50" class="online" alt="" />
-				<p><?=$userProfile['name'].' '.$userProfile['lastName']?></p>
+				<p><?= $userProfile['name'] . ' ' . $userProfile['lastName'] ?></p>
 			</div>
 		</div>
 		<div id="search">
@@ -16,9 +16,9 @@ ob_start();
 		</div>
 		<div id="contacts">
 			<ul>
-            <?php  for ($i=0;$i<count($contactProfile);$i++):?>
+            <?php for ($i = 0; $i < count($contactProfile); $i++) : ?>
 			<form action="index.php?action=showMessages" method="POST">
-			<input type="hidden" name="contactId" value="<?=$contactProfile[$i]['id']?>">
+			<input type="hidden" name="contactId" value="<?= $contactProfile[$i]['id'] ?>">
 
 				<li class="contact">
 					<div class="wrap">
@@ -27,10 +27,10 @@ ob_start();
 						<div class="meta">
 						<button type="submit" class="btn btn-link">
 
-							<p class="name"><?= $contactProfile[$i]['name'] .' '. $contactProfile[$i]['lastName'] ?></p>
+							<p class="name"><?= $contactProfile[$i]['name'] . ' ' . $contactProfile[$i]['lastName'] ?></p>
 							</button>
 
-							<p class="preview"><?= "É c koman la ca di koi "?></p>
+							<p class="preview"><?= "É c koman la ca di koi " ?></p>
 
 							</form>
 						</div>
@@ -46,46 +46,77 @@ ob_start();
 	<div class="content">
 		<div class="contact-profile">
 			<img src="https://picsum.photos/50/50" alt="" />
-			<p><?= $reiceverProfile['name'].' '.$reiceverProfile['lastName']?></p>
-		</div>
+			<p><?= $reiceverProfile['name'] . ' ' . $reiceverProfile['lastName'] ?></p>
+        </div>
+        
 		<div class="messages">
-			<ul>
+			<ul id="messages">
 			 <?php 
-					while($messagesFetch = $messages->fetch()) :
-						if ($messagesFetch['reicever']== $_SESSION['id']) {
-							$class = "sent";
-						} else {
-							$class = "replies";
-						}
-				?>
-				<li class=<?=$class?>>
-					<p><?=$messagesFetch['content']?></p>
-					<?php endwhile ?>
+			while ($messagesFetch = $messages->fetch()) :
+				if ($messagesFetch['reicever'] == $_SESSION['id']) {
+				$class = "sent";
+			} else {
+				$class = "replies";
+			}
+			?>
+				<li class=<?= $class ?>>
+					<p><?= $messagesFetch['content'] ?></p>
+                    <?php endwhile ?>
 			</ul>
 		</div>
 		<div class="message-input">
 			<div class="wrap">
 			<form action="index.php?action=sendMessage" method="POST">
-				<input type="text" name="content" placeholder="Écrivez votre message" />
-				<input type="hidden" name="contactId" value="<?=$_POST['contactId']?>">
-				<i class="fa fa-paperclip attachment" aria-hidden="true"></i>
-				<button type="submit"><i class="fa fa-paper-plane" aria-hidden="true"></i></button>
+				<input type="text" name="content" id="content" placeholder="Écrivez votre message" />
+				<input type="hidden" name="contactId" id="contactId" value="<?= $_POST['contactId'] ?>">
+				<i class="fa fa-paperclip attachment" aria-hidden="true"></i> 
+				<button type="submit" id="send"><i class="fa fa-paper-plane" aria-hidden="true"></i></button>
 			</form>
 			</div>
 		</div>
 	</div>
 </div>
 <script src='//production-assets.codepen.io/assets/common/stopExecutionOnTimeout-b2a7b3fe212eaa732349046d8416e00a9dec26eb7fd347590fbced3ab38af52e.js'></script><script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
-<script >$(".messages").animate({ scrollTop: $(document).height() }, "fast");
+<script >$("#messages").animate({ scrollTop: $(document).height() }, "fast");
 //BARRE DE SCROLL EN BAS 
-$(window).on('keydown', function(e) {
-  if (e.which == 13) {
-    return false;
-  }
-});
-//# sourceURL=pen.js
+	$(window).on('keydown', function(e) {
+	if (e.which == 13) {
+		return false;
+	}
+
+	$('#envoi').click(function(e)){
+		e.preventDefault();
+		var content = $('#content').val();
+		var contactId = $('#contactId').val();
+
+		if (content != "" && trim(content) != "") {
+			$.ajax({
+				url : "index.php?action=sendMessage",
+				type : "POST",
+				data : "content=" + content + "&contactId=" + idContact
+			});
+			$('#messages').append("<li class=<?= $class ?>><p>" + content + "</p></ul>");
+		}
+	}
+	});
+function load(){
+		setTimeout( function(){
+			var fisrtId = $('#messages p:first').attr('id');
+			$.ajax({
+				url : "index.php?action=showMessages",
+				type : POST,
+				data : "messageId=" + firstId,
+				success : function(html){
+					$('#messages').prepend(html);
+				}
+			})
+		});
+		load();
+	},5000);
+load();
+
 </script>
  <?php 
-    $content = ob_get_clean();
-    require('./view/template.php');
- ?>
+$content = ob_get_clean();
+require('./view/template.php');
+?>
