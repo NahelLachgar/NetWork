8b72348e4a8d7f77920340c8f525847c05fd5edb
NahelLachgar <nahellachgar@hotmail.fr>
2019-01-12 16:00:59 +0100

    ajout photo groupe bdd

--- bdd/create.sql
+++ bdd/create.sql
@@ -159,6 +159,7 @@ CREATE TABLE IF NOT EXISTS `NetWork`.`groups` (
   `title` VARCHAR(45) NOT NULL,
   `createDate` DATE NULL,
   `admin` INT NOT NULL,
+    `photo` VARCHAR(255) NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) ,
   UNIQUE INDEX `titre_UNIQUE` (`title` ASC) )
