c37cd84010409673058594e21621029f4978f90d
NahelLachgar <nahellachgar@hotmail.fr>
2019-01-19 14:32:55 +0100

    bdd

--- view/chatView.php
+++ view/chatView.php
@@ -81,7 +81,7 @@ ob_start();
 			<form>
 				<input type="text" name="content" id="content" placeholder="Écrivez votre message" />
 				<input type="hidden" name="contactId" id="contactId" value=<?= $_GET['contactId'] ?>>
-				<img style="position:relative; left:1%;top:5%" width="2%" src="img/icon/imageFile.svg"> 
+				<i class="fa fa-paperclip attachment" aria-hidden="true"></i>
 				<button type="button" id="send"><i class="fa fa-paper-plane" aria-hidden="true"></i></button>
 			</form>
 			</div>
