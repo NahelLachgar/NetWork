53a7ae757acfe10c8e65cee6756ffa8de63e36a6
NahelLachgar <nahellachgar@hotmail.fr>
2019-02-04 00:13:45 +0100

    bdd hebergeur

--- model/dbConnect.php
+++ model/dbConnect.php
@@ -2,7 +2,7 @@
 function dbConnect()
 {
     try {
-        $db = new PDO('mysql:host=localhost;dbname=NetWork;charset=utf8', 'root', 'root');
+        $db = new PDO('mysql:host=localhost;dbname=u503126698_arar;charset=utf8', 'u503126698_arar', 'motdepasse123');
     } catch (Exception $e) {
         die('Erreur : ' . $e->getMessage());
     }
